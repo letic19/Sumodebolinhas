13772b5a5517b5043af3baf5ca1f2e06b707c7b9
Judsonnn <145375544+Judsonnn@users.noreply.github.com>
2026-07-02 17:34:00 -0300

    Ajust button
    Os botoes estao com problemas, nao passa a bola e nem da pra selecionar

--- Sumodebolinhas/Assets/Scripts/Gameplay/CharacterSelectionManager.cs
+++ Sumodebolinhas/Assets/Scripts/Gameplay/CharacterSelectionManager.cs
@@ -8,41 +8,129 @@ public class CharacterSelectionManager : MonoBehaviour
     [SerializeField] private BolinhaData[] bolinhas;
 
     [Header("Interface Jogador 1")]
-    [SerializeField] private TMP_Text nomeBolinha;
-    [SerializeField] private Image imagemBolinha;
+    [SerializeField] private TMP_Text nomeBolinhaP1;
+    [SerializeField] private Image imagemBolinhaP1;
 
-    private int indiceAtual = 0;
+    [Header("Interface Jogador 2")]
+    [SerializeField] private TMP_Text nomeBolinhaP2;
+    [SerializeField] private Image imagemBolinhaP2;
+
+    private int indiceJogador1 = 0;
+    private int indiceJogador2 = 0;
+
+    private bool jogador1Confirmou = false;
+    private bool jogador2Confirmou = false;
 
     private void Start()
     {
-        AtualizarInterface();
+        AtualizarInterfaceJogador1();
+        AtualizarInterfaceJogador2();
     }
 
-    public void Proximo()
+    // ---------------- JOGADOR 1 ----------------
+
+    public void ProximoJogador1()
     {
-        indiceAtual++;
+        indiceJogador1++;
 
-        if (indiceAtual >= bolinhas.Length)
-            indiceAtual = 0;
+        if (indiceJogador1 >= bolinhas.Length)
+            indiceJogador1 = 0;
 
-        AtualizarInterface();
+        AtualizarInterfaceJogador1();
     }
 
-    public void Anterior()
+    public void AnteriorJogador1()
     {
-        indiceAtual--;
+        indiceJogador1--;
 
-        if (indiceAtual < 0)
-            indiceAtual = bolinhas.Length - 1;
+        if (indiceJogador1 < 0)
+            indiceJogador1 = bolinhas.Length - 1;
 
-        AtualizarInterface();
+        AtualizarInterfaceJogador1();
     }
 
-    private void AtualizarInterface()
+    private void AtualizarInterfaceJogador1()
     {
-        nomeBolinha.text = bolinhas[indiceAtual].name;
+        Debug.Log("Quantidade: " + bolinhas.Length);
+        Debug.Log("Índice: " + indiceJogador1);
 
-        if (bolinhas[indiceAtual].icone != null)
-            imagemBolinha.sprite = bolinhas[indiceAtual].icone;
+        if (nomeBolinhaP1 == null)
+        {
+            Debug.LogError("NomeBolinhaP1 não foi configurado!");
+            return;
+        }
+
+        if (bolinhas[indiceJogador1] == null)
+        {
+            Debug.LogError("Elemento da bolinha é nulo!");
+            return;
+        }
+
+        nomeBolinhaP1.text = bolinhas[indiceJogador1].name;
+
+        if (imagemBolinhaP1 != null && bolinhas[indiceJogador1].icone != null)
+        {
+            imagemBolinhaP1.sprite = bolinhas[indiceJogador1].icone;
+        }
+
+        Debug.Log("Atualizou Jogador 1");
+    }
+
+    public void ConfirmarJogador1()
+    {
+        jogador1Confirmou = true;
+
+        Debug.Log("Jogador 1 escolheu: " + bolinhas[indiceJogador1].name);
+
+        VerificarInicio();
+    }
+
+    // ---------------- JOGADOR 2 ----------------
+
+    public void ProximoJogador2()
+    {
+        indiceJogador2++;
+
+        if (indiceJogador2 >= bolinhas.Length)
+            indiceJogador2 = 0;
+
+        AtualizarInterfaceJogador2();
+    }
+
+    public void AnteriorJogador2()
+    {
+        indiceJogador2--;
+
+        if (indiceJogador2 < 0)
+            indiceJogador2 = bolinhas.Length - 1;
+
+        AtualizarInterfaceJogador2();
+    }
+
+    private void AtualizarInterfaceJogador2()
+    {
+        nomeBolinhaP2.text = bolinhas[indiceJogador2].name;
+
+        if (bolinhas[indiceJogador2].icone != null)
+            imagemBolinhaP2.sprite = bolinhas[indiceJogador2].icone;
+    }
+
+    public void ConfirmarJogador2()
+    {
+        jogador2Confirmou = true;
+
+        Debug.Log("Jogador 2 escolheu: " + bolinhas[indiceJogador2].name);
+
+        VerificarInicio();
+    }
+
+    // ---------------- VERIFICA ----------------
+
+    private void VerificarInicio()
+    {
+        if (jogador1Confirmou && jogador2Confirmou)
+        {
+            Debug.Log("Os dois jogadores confirmaram!");
+        }
     }
 }
